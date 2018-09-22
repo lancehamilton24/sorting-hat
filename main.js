6e85274433e4f899ced5390279c7d3341b1b85d8
Lance Hamilton <lance.hamilton24@gmail.com>
2018-09-22 09:37:57 -0500

    added student form function to hide and show onclick

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-const printToDom = (stringToPrint, elementId) => {
-    const selectedDiv = document.getElementById(elementId);
-    selectedDiv.innerHTML = stringToPrint;
-};
+// const printToDom = (stringToPrint, elementId) => {
+//     const selectedDiv = document.getElementById(elementId);
+//     selectedDiv.innerHTML = stringToPrint;
+// };
 
+var button = document.getElementById('startButton'); 
+
+button.onclick = function() {
+    var div = document.getElementById('collapseExample');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    }
+    else {
+        div.style.display = 'block';
+    }
+};
